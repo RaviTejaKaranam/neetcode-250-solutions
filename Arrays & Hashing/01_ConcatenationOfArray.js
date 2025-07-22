de6977916f4ed1480d27c843347d79b110dd9fd6
Ravi Teja Karanam <50255591+RaviTejaKaranam@users.noreply.github.com>
2025-07-22 11:51:22 -0400

    Update 01_ConcatenationOfArray.js

--- Arrays & Hashing/01_ConcatenationOfArray.js
+++ Arrays & Hashing/01_ConcatenationOfArray.js
@@ -4,6 +4,7 @@ class Solution {
      * @return {number[]}
      */
     getConcatenation(nums) {
+        //Using spread operator to create a new array with nums followed by nums again
         return [...nums, ...nums]
     }
 }
