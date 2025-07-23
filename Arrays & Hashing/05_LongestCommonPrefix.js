ee4be9834956f0c9d289b473c4d77891c4c3d299
Ravi Teja Karanam <50255591+RaviTejaKaranam@users.noreply.github.com>
2025-07-23 09:11:54 -0400

    Create 05_LongestCommonPrefix.js

---
+++ Arrays & Hashing/05_LongestCommonPrefix.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+
+        let commonPrefix = ""
+
+        //Sort the strs array
+        strs.sort()
+
+        //Max possible common prefix length
+        let maxPossibleLen = Math.min(strs[0].length, strs[strs.length - 1].length)
+
+        for(let i = 0; i < maxPossibleLen; i++){
+            //Check if there is a char mismatch between first and the last string
+            //Return the commonPrefix if there is a mismatch
+            if(strs[0][i] !== strs[strs.length - 1][i]){
+                return commonPrefix
+            }
+            //If the char is equal, it is part of the commonPrefix
+            else{
+                commonPrefix += strs[0][i]
+            }
+        }
+
+    }
+}
