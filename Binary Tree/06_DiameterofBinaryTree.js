f47c5fc0beee43ab6bb8804fffadca73b5227e83
Ravi Teja Karanam <50255591+RaviTejaKaranam@users.noreply.github.com>
2026-05-02 11:28:38 +0530

    Implement diameterOfBinaryTree function

---
+++ Binary Tree/06_DiameterofBinaryTree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    diameterOfBinaryTree(root) {
+        let diameter = 0;
+        const helper = (node) => {
+            if(!node) return 0;
+            let leftDepth = helper(node.left);
+            let rightDepth = helper(node.right);
+
+            diameter = Math.max(leftDepth + rightDepth, diameter);
+            return 1 + Math.max(leftDepth, rightDepth);
+        }
+
+        helper(root);
+        return diameter;
+    }
+}
